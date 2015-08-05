772ed2f9c94d3889af2933bb11b91e841edf103d
Kuitos <kuitos.lau@gmail.com>
2015-08-05 22:11:15 +0800

    format

--- services/http-handler.js
+++ services/http-handler.js
@@ -6,156 +6,156 @@
  */
 ;
 (function (angular, undefined) {
-    "use strict";
+  "use strict";
 
-    // 模拟service的私有服务
-    var _app = {};
+  // 模拟service的私有服务
+  var _app = {};
 
-    angular.module("ngUtils.services.httpHandler", [])
+  angular.module("ngUtils.services.httpHandler", [])
 
-        .config(["$httpProvider", function ($httpProvider) {
+    .config(["$httpProvider", function ($httpProvider) {
 
-            var
-                /** http请求相关状态(loading,saving)切换 */
-                count = 0,
-                loading = false,
-                saving = false,
+      var
+        /** http请求相关状态(loading,saving)切换 */
+        count = 0,
+        loading = false,
+        saving = false,
 
-                stopLoading = function () {
-                    loading = false;
-                    _app.isLoading(false); // end loading
-                },
-                stopSaving = function () {
-                    saving = false;
-                    _app.isSaving(false); // end saving
-                };
+        stopLoading = function () {
+          loading = false;
+          _app.isLoading(false); // end loading
+        },
+        stopSaving = function () {
+          saving = false;
+          _app.isSaving(false); // end saving
+        };
 
-            /*************************** http超时时间设为30s ***************************/
-            $httpProvider.defaults.timeout = 30 * 1000;
-            /*************************** 禁用浏览器缓存 ***************************/
-            $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
-            /* 广告时间哈哈.... */
-            $httpProvider.defaults.headers.common["X-Requested-With"] = "https://github.com/kuitos";
+      /*************************** http超时时间设为30s ***************************/
+      $httpProvider.defaults.timeout = 30 * 1000;
+      /*************************** 禁用浏览器缓存 ***************************/
+      $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
+      /* 广告时间哈哈.... */
+      $httpProvider.defaults.headers.common["X-Requested-With"] = "https://github.com/kuitos";
 
-            /******************** http拦截器，用于统一处理错误信息、消息缓存、请求响应状态、响应结果处理等 **********************/
-            $httpProvider.interceptors.push(["$q", "$log", "$timeout", "$cacheFactory", "tipsHandler",
-                function ($q, $log, $timeout, $cacheFactory, tipsHandler) {
+      /******************** http拦截器，用于统一处理错误信息、消息缓存、请求响应状态、响应结果处理等 **********************/
+      $httpProvider.interceptors.push(["$q", "$log", "$timeout", "$cacheFactory", "tipsHandler",
+        function ($q, $log, $timeout, $cacheFactory, tipsHandler) {
 
-                    return {
+          return {
 
-                        request: function (config) {
+            request: function (config) {
 
-                            count++;
-                            if (!loading) {
+              count++;
+              if (!loading) {
 
-                                // saving start
-                                if (config.savingStatus) {
-                                    saving = true;
-                                    _app.isSaving(true);
-                                }
-
-                                $timeout(function () {
-                                    if (!loading && count > 0) {
-                                        loading = true;
-                                        _app.isLoading(true);
-                                    }
-                                }, 500); // if no response in 500ms, begin loading
-                            }
-
-                            return config;
-                        },
-
-                        requestError: function (rejection) {
-                            $log.error("%s 接口请求失败!", rejection.url);
-                            return $q.reject(rejection);
-                        },
-
-                        response: function (res) {
-                            var config = res.config,
-                                responseBody = res.data,
-                                cache;
-
-                            count--;
-                            // 响应结束，清除相关状态
-                            if (count === 0) {
-                                stopSaving();
-                                if (loading) {
-                                    stopLoading();
-                                }
-                            }
-
-                            // rest接口响应
-                            if (angular.isObject(responseBody) || res.status > 200) {
-
-                                // 自定义配置，若该请求成功后需要重新刷新cache(save,update,delete等操作)，则清空对应cache。默认cache为defaultRestCache
-                                if (config.refreshCache) {
-                                    cache = angular.isObject(config.cache) ? config.cache : $cacheFactory.get("defaultRestCache");
-                                    cache.removeAll();
-                                }
-
-                                // 关注保存状态则弹出成功提示
-                                if (config.savingStatus) {
-                                    tipsHandler.success(responseBody.message);
-                                }
-                            }
-
-                            return res;
-                        },
-
-                        responseError: function (rejection) {
-
-                            count--;
-                            // 响应结束，清除相关状态
-                            if (count === 0) {
-                                stopSaving();
-                                if (loading) {
-                                    stopLoading();
-                                }
-                            }
-
-                            // 失败弹出错误提示信息
-                            tipsHandler.error("请求错误!");
-                            $log.error("接口 %s 请求错误! 状态：%s 错误信息：%s", rejection.config.url, rejection.status, rejection.statusText);
-                            return $q.reject(rejection);
-                        }
-                    }
-                }]);
-        }])
-
-        /* 提示信息provider，用于配置错误提示处理器 **/
-        .provider("tipsHandler", function () {
-
-            var _tipsHandler = {
-                error  : angular.noop,
-                warning: angular.noop,
-                success: angular.noop
-            };
-
-            this.setTipsHandler = function (tipsHandler) {
-                _tipsHandler = angular.extend(_tipsHandler, tipsHandler);
-            };
-
-            this.$get = function () {
-                return {
-                    error  : _tipsHandler.error,
-                    warning: _tipsHandler.warning,
-                    success: _tipsHandler.success
+                // saving start
+                if (config.savingStatus) {
+                  saving = true;
+                  _app.isSaving(true);
                 }
-            };
 
-        })
+                $timeout(function () {
+                  if (!loading && count > 0) {
+                    loading = true;
+                    _app.isLoading(true);
+                  }
+                }, 500); // if no response in 500ms, begin loading
+              }
 
-        .run(["$rootScope", function ($rootScope) {
+              return config;
+            },
 
-            /** loading状态切换 **/
-            _app.isLoading = function (flag) {
-                $rootScope.loading = flag;
-            };
+            requestError: function (rejection) {
+              $log.error("%s 接口请求失败!", rejection.url);
+              return $q.reject(rejection);
+            },
 
-            _app.isSaving = function (flag) {
-                $rootScope.saving = flag;
-            };
+            response: function (res) {
+              var config = res.config,
+                responseBody = res.data,
+                cache;
 
+              count--;
+              // 响应结束，清除相关状态
+              if (count === 0) {
+                stopSaving();
+                if (loading) {
+                  stopLoading();
+                }
+              }
+
+              // rest接口响应
+              if (angular.isObject(responseBody) || res.status > 200) {
+
+                // 自定义配置，若该请求成功后需要重新刷新cache(save,update,delete等操作)，则清空对应cache。默认cache为defaultRestCache
+                if (config.refreshCache) {
+                  cache = angular.isObject(config.cache) ? config.cache : $cacheFactory.get("defaultRestCache");
+                  cache.removeAll();
+                }
+
+                // 关注保存状态则弹出成功提示
+                if (config.savingStatus) {
+                  tipsHandler.success(responseBody.message);
+                }
+              }
+
+              return res;
+            },
+
+            responseError: function (rejection) {
+
+              count--;
+              // 响应结束，清除相关状态
+              if (count === 0) {
+                stopSaving();
+                if (loading) {
+                  stopLoading();
+                }
+              }
+
+              // 失败弹出错误提示信息
+              tipsHandler.error("请求错误!");
+              $log.error("接口 %s 请求错误! 状态：%s 错误信息：%s", rejection.config.url, rejection.status, rejection.statusText);
+              return $q.reject(rejection);
+            }
+          }
         }]);
+    }])
+
+    /* 提示信息provider，用于配置错误提示处理器 **/
+    .provider("tipsHandler", function () {
+
+      var _tipsHandler = {
+        error  : angular.noop,
+        warning: angular.noop,
+        success: angular.noop
+      };
+
+      this.setTipsHandler = function (tipsHandler) {
+        _tipsHandler = angular.extend(_tipsHandler, tipsHandler);
+      };
+
+      this.$get = function () {
+        return {
+          error  : _tipsHandler.error,
+          warning: _tipsHandler.warning,
+          success: _tipsHandler.success
+        }
+      };
+
+    })
+
+    .run(["$rootScope", function ($rootScope) {
+
+      /** loading状态切换 **/
+      _app.isLoading = function (flag) {
+        $rootScope.loading = flag;
+      };
+
+      _app.isSaving = function (flag) {
+        $rootScope.saving = flag;
+      };
+
+    }]);
 
 })(window.angular);
