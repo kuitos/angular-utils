4cd8f19ca5d4a322da9d58308419896f5ec442a9
Kuitos <kuitos.lau@gmail.com>
2015-11-20 15:05:18 +0800

    debounce工具方法bugxiuf & throttle方法优化

--- utils/base-utils.js
+++ utils/base-utils.js
@@ -8,14 +8,16 @@
 
   'use strict';
 
+  var _services = {};
+
   // 性能工具
   var PerformanceUtils = {
 
     /**
-     * 函数只会在确定时间延时后才会被触发
+     * 函数只会在确定时间延时后才会被触发(只会执行间隔时间内的最后一次调用)
      */
     debounce: function (func, delay, scope, invokeApply) {
-      var $timeout = _service.$timeout,
+      var $timeout = _services.$timeout,
         timer;
 
       return function debounced() {
@@ -33,13 +35,13 @@
     },
 
     /**
-     * 函数节流。使一个持续性触发的函数执行间隔大于指定时间才会有效
+     * 函数节流。使一个持续性触发的函数执行间隔大于指定时间才会有效(只会执行间隔时间内的第一次调用)
      */
     throttle: function throttle(func, delay, context) {
       var recent;
       return function throttled() {
-        var context = context || null,
-          now = Date.now();
+        var now = Date.now();
+        context = context || null;
 
         if (!recent || (now - recent > (delay || 10))) {
           func.apply(context, arguments);
@@ -53,6 +55,9 @@
     .module('ngUtils.utils.baseUtils', [])
     .constant('baseUtils', {
       PerformanceUtils: PerformanceUtils
+    })
+    .run('$timeout', function ($timeout) {
+      _services.$timeout = $timeout;
     });
 
 })(window.angular);
